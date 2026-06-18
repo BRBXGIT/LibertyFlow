5e2aa9d00d697aee1eabc7b4caf6b3c455c5bcb7
BRBX <egorka2005sautkin@gmail.com>
2026-06-18 19:00:14 +0300

    Add core & feature containers

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     // Application
     alias(libs.plugins.android.application) apply false
     // Compose
     alias(libs.plugins.kotlin.compose) apply false
+    // Kotlin jvm
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+    // Android library
+    alias(libs.plugins.android.library) apply false
+    // Serialization
+    alias(libs.plugins.kotlin.serialization) apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,9 +12,11 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -25,4 +27,12 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "LibertyFlow"
+
+// App
 include(":app")
+
+// Core
+include(":core")
+
+// Features
+include(":feature")
