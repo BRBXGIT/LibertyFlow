c57e4610acbf31b82ef5e4056568542f18c5aa0f
BRBX <egorka2005sautkin@gmail.com>
2026-07-09 16:22:02 +0300

    Add animation to anime card

--- core/design-system/src/main/java/com/brbx/design_system/component/anime_card/AnimeCard.kt
+++ core/design-system/src/main/java/com/brbx/design_system/component/anime_card/AnimeCard.kt
@@ -1,11 +1,15 @@
 package com.brbx.design_system.component.anime_card
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.brbx.coil_helpers.helpers.BrbxRemoteImage
 import com.brbx.ui_compose.common.BrbxText
@@ -13,6 +17,7 @@ import com.brbx.ui_compose.components.complex.content_card.content_card.BrbxCont
 import com.brbx.ui_compose.components.complex.content_card.content_card.BrbxContentCardAppearance
 import com.brbx.ui_compose.components.complex.content_card.content_card.rememberCopy
 import com.brbx.ui_compose.theme.bAnimationTokens
+import com.brbx.ui_compose.theme.bMotion
 
 @Composable
 fun AnimeCard(
@@ -25,13 +30,22 @@ fun AnimeCard(
     selected: Boolean = false,
     appearance: BrbxContentCardAppearance = AnimeCardConstants.AnimeCardAppearance,
 ) {
-    val finalAppearance = if (selected) {
-        appearance.rememberCopy(
-            containerBorder = {
-                BorderStroke(width = 3.dp, color = AnimeCardConstants.selectedBorderColor)
-            }
-        )
-    } else appearance
+    val selectedColor = AnimeCardConstants.selectedBorderColor
+    val animatedBorderColor by animateColorAsState(
+        targetValue = if (selected) selectedColor else Color.Transparent,
+        animationSpec = bMotion.nonSpatialFastSpec(),
+        label = "Selected border color animation"
+    )
+    val animatedBorderStroke by animateDpAsState(
+        targetValue = if (selected) 3.dp else 0.dp,
+        animationSpec = bMotion.mediumSpatialSpec(),
+        label = "Selected border width animation"
+    )
+    val finalAppearance = appearance.rememberCopy(
+        containerBorder = {
+            BorderStroke(width = animatedBorderStroke, color = animatedBorderColor)
+        }
+    )
     BrbxContentCard(
         modifier = modifier
             .size(AnimeCardConstants.Width, AnimeCardConstants.Height)
