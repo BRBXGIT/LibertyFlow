e6c67ebf4f8bd0cbe307aef2c6760c1b5a0398fa
BRBX <egorka2005sautkin@gmail.com>
2026-07-18 16:53:25 +0300

    Add loading snackbar need to test

--- feature/common/src/main/java/com/brbx/common/view_model/processor/selection/processor/CommonSelectionProcessorImpl.kt
+++ feature/common/src/main/java/com/brbx/common/view_model/processor/selection/processor/CommonSelectionProcessorImpl.kt
@@ -1,6 +1,8 @@
 package com.brbx.common.view_model.processor.selection.processor
 
+import androidx.annotation.StringRes
 import arrow.optics.Lens
+import com.brbx.common.model.alias.CommonStrings
 import com.brbx.common.model.common.map.toBrbxText
 import com.brbx.common.utils.toggle
 import com.brbx.common.view_model.processor.selection.model.CommonSelectionIntent
@@ -18,8 +20,14 @@ import com.brbx.domain.network.user.lists.collections.collections.use_case.UserA
 import com.brbx.domain.network.user.lists.collections.collections.use_case.UserDeleteFromCollectionUseCase
 import com.brbx.domain.network.user.lists.favorites.favorites.use_case.UserAddToFavoritesUseCase
 import com.brbx.domain.network.user.lists.favorites.favorites.use_case.UserDeleteFromFavoritesUseCase
+import com.brbx.mvi_compose.effects.BrbxEffect
+import com.brbx.ui_compose.common.toBrbxText
+import com.brbx.ui_compose.components.complex.snackbar.common.BrbxSnackbarDuration
+import com.brbx.ui_compose.components.complex.snackbar.loading_snackbar.config.DefaultBrbxLoadingSnackbarConfig
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.time.Duration.Companion.milliseconds
 
 internal class CommonSelectionProcessorImpl<State>(
     private val addToFavoritesUseCase: UserAddToFavoritesUseCase,
@@ -56,7 +64,13 @@ internal class CommonSelectionProcessorImpl<State>(
     }
 
     private fun LibertyFlowMviScope<State>.processFavorites(intent: CommonSelectionIntent.Lists.Favorites) {
-        executeRequest(intent) { ids ->
+        executeRequest(
+            intent = intent,
+            loadingSnackbarRes = when (intent.action) {
+                SelectionAction.Add -> CommonStrings.adding_to_favorites
+                SelectionAction.Remove -> CommonStrings.deleting_from_favorites
+            }
+        ) { ids ->
             when (intent.action) {
                 SelectionAction.Add -> addToFavoritesUseCase(items = ids)
                 SelectionAction.Remove -> deleteFromFavoritesUseCase(items = ids)
@@ -67,7 +81,13 @@ internal class CommonSelectionProcessorImpl<State>(
     private fun LibertyFlowMviScope<State>.processCollection(intent: CommonSelectionIntent.Lists.Collection) {
         when (intent) {
             is CommonSelectionIntent.Lists.Collection.Interact -> {
-                executeRequest(intent) { ids ->
+                executeRequest(
+                    intent = intent,
+                    loadingSnackbarRes = when (intent.action) {
+                        SelectionAction.Add -> CommonStrings.adding_to_collection
+                        SelectionAction.Remove -> CommonStrings.deleting_from_collection
+                    }
+                ) { ids ->
                     val items = ids.map { CollectionItem(id = it, collection = intent.collection) }
                     when (intent.action) {
                         SelectionAction.Add -> addToCollectionUseCase(items = items)
@@ -88,20 +108,35 @@ internal class CommonSelectionProcessorImpl<State>(
 
     private fun LibertyFlowMviScope<State>.executeRequest(
         intent: CommonSelectionIntent,
+        @StringRes loadingSnackbarRes: Int,
         request: suspend (List<Int>) -> DomainRequestResult<Unit>,
     ) {
         val selectedIds = selectionLens.get(state.value).ids.toList()
         if (selectedIds.isEmpty()) return
 
+        val loadingSnackbarId = "loading_snackbar_id"
         coroutineScope.launch(context = dispatcherIo) {
             updateState { selectionLens.modify(source = this) { it.copy(ids = emptySet()) } }
+            postCommonEffect(
+                BrbxEffect.ShowSnackbar(
+                    config = DefaultBrbxLoadingSnackbarConfig(
+                        id = loadingSnackbarId,
+                        text = loadingSnackbarRes.toBrbxText(),
+                        duration = BrbxSnackbarDuration.Infinite,
+                        isDismissable = false,
+                    )
+                )
+            )
+            delay(duration = 2_000.milliseconds) // Animation delay for snackbar (antipattern, maybe will be rewritten)
 
             makeNetworkCall(
                 loadingLens = selectionLens.loadingState,
                 call = { request(selectedIds) }
             ).onSuccess {
+                postCommonEffect(BrbxEffect.RemoveSnackbarById(loadingSnackbarId))
                 // TODO
             } onException { exception ->
+                postCommonEffect(BrbxEffect.RemoveSnackbarById(loadingSnackbarId))
                 updateState { selectionLens.modify(source = this) { it.copy(ids = selectedIds.toSet()) } }
                 postExceptionSnackbar(
                     exception = exception.toBrbxText(),
